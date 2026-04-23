ad27a0fb1bc71b3821d286ddbbb900274223eef6
Ali Kashefi <58951525+Ali-Stanford@users.noreply.github.com>
2026-04-23 10:25:57 -0700

    Update test.py

--- segmentation/test.py
+++ segmentation/test.py
@@ -8,11 +8,13 @@
 #[PointNet with KAN versus PointNet with MLP for 3D Classification and Segmentation of Point Sets]
 # https://doi.org/10.1016/j.cag.2025.104319
 
-#@article{kashefi2024PointNetKAN,
-#title={PointNet with KAN versus PointNet with MLP for 3D Classification and Segmentation of Point Sets},
+#@article{kashefi2025PointNetKANgraphics,
+#title={PointNet with KAN versus PointNet with MLP for 3D classification and segmentation of point sets},
 #author={Kashefi, Ali},
-#journal={arXiv preprint arXiv:2410.10084},
-#year={2024}}
+#journal={Computers \& Graphics},
+#pages={104319},
+#year={2025},
+#publisher={Elsevier}}
 
 # Libraries
 import os
